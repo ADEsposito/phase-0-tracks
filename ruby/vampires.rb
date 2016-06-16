f976baaad736471fafa46ce4571bf2c190fa0f0e
ADEsposito <a.desposito640@gmail.com>
2016-06-15 20:57:48 -0500

    initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,9 @@
+# What is your name
+
+# How old are you?
+
+# What year were you born?
+
+# Our company serves garlic bread. Should we order some for you?
+
+# Would you like to enroll in the company's health insurance?
