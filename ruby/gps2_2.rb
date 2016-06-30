170ccdec5cd1a82e398506bd62860a9411617cad
ADEsposito <a.desposito640@gmail.com>
2016-06-30 14:56:48 -0500

    add working code, also add code for method to get user input, add example of test run

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -41,3 +41,206 @@
   # loop through hash
     # print item (key) and quantity (value) in manner that's easy for user to read
 # output: print out of key/value pairs in easy to read manner
+
+$detailed_groceries = {}
+
+def print_list(hash)
+	p "GROCERY LIST"
+	$detailed_groceries.each do |item, quantity|
+		p "#{item} : #{quantity}"
+	end
+end
+
+def create_list(grocery_items)
+	grocery_array = grocery_items.split(' ')
+	grocery_array.each { |item| $detailed_groceries[item] = 1 }
+	print_list($detailed_groceries)
+	return $detailed_groceries
+end
+
+def add_to_list(hash, item, quantity = 1)
+	$detailed_groceries[item] = quantity
+	return $detailed_groceries
+end
+
+def remove_from_list(hash, item)
+	$detailed_groceries.delete(item)
+	return $detailed_groceries
+end
+
+def update_list(hash, item, quantity)
+	$detailed_groceries[item] = quantity
+	return $detailed_groceries
+end
+
+
+def get_input
+	exit = false
+
+	until exit
+		puts "- To CREATE a list press 'C'"
+		puts "- To ADD to your list press 'A'"
+		puts "- To REMOVE an item from your list press 'R'"
+		puts "- To UPDATE the quantity of an item press 'U'"
+		puts "- To PRINT your list press 'P'"
+		puts "- To EXIT press 'E"
+
+		choice = gets.chomp
+
+		if choice == 'C'
+			puts "Please enter the items you wish to add, separating each item with a space (example: carrots apples cereal pizza) :"
+			grocery_items = gets.chomp
+			create_list(grocery_items)
+			exit = false
+		end
+
+		if choice == 'A'
+			puts "What item would you like to add?"
+			item = gets.chomp
+      puts "How many would you like to add?"
+      quantity = gets.chomp
+			add_to_list(hash, item, quantity)
+			puts "#{item} has been added with a quantity of #{quantity}."
+			exit = false
+		end
+
+		if choice == 'R'
+			puts "What item would you like to remove?"
+			item = gets.chomp
+			remove_from_list(hash, item)
+			puts "#{item} has been removed from your list."
+			exit = false
+		end
+
+		if choice == 'U'
+			puts "What item would you like to update?"
+			item = gets.chomp
+			puts "What is the new quantity you would like for #{item}?"
+			quantity = gets.chomp
+			update_list(hash, item, quantity)
+			puts "#{item} has been updated to have a quantity of #{quantity}."
+			exit = false
+		end
+
+		if choice == 'P'
+			print_list(hash)
+			exit = false
+		end
+
+		if choice == 'E'
+			puts "Thank you for using 'GROCERY LIST'"
+			exit = TRUE
+		end
+	end
+end
+
+get_input
+
+
+# Test run using iTerm
+#ruby :> ruby gps2_2.rb
+#- To CREATE a list press 'C'
+#- To ADD to your list press 'A'
+#- To REMOVE an item from your list press 'R'
+#- To UPDATE the quantity of an item press 'U'
+#- To PRINT your list press 'P'
+#- To EXIT press 'E
+#C
+#Please enter the items you wish to add, separating each item with a space (example: carrots apples cereal pizza) :
+#apple cereal
+#"GROCERY LIST"
+#"apple : 1"
+#"cereal : 1"
+#- To CREATE a list press 'C'
+#- To ADD to your list press 'A'
+#- To REMOVE an item from your list press 'R'
+#- To UPDATE the quantity of an item press 'U'
+#- To PRINT your list press 'P'
+#- To EXIT press 'E
+#A
+#What item would you like to add?
+#lemonade
+#How many would you like to add?
+#2
+#lemonade has been added with a quantity of 2.
+#- To CREATE a list press 'C'
+#- To ADD to your list press 'A'
+#- To REMOVE an item from your list press 'R'
+#- To UPDATE the quantity of an item press 'U'
+#- To PRINT your list press 'P'
+#- To EXIT press 'E
+#A
+#What item would you like to add?
+#tomatoes
+#How many would you like to add?
+#3
+#tomatoes has been added with a quantity of 3.
+#- To CREATE a list press 'C'
+#- To ADD to your list press 'A'
+#- To REMOVE an item from your list press 'R'
+#- To UPDATE the quantity of an item press 'U'
+#- To PRINT your list press 'P'
+#- To EXIT press 'E
+#A
+#What item would you like to add?
+#onions
+#How many would you like to add?
+#1
+#onions has been added with a quantity of 1.
+#- To CREATE a list press 'C'
+#- To ADD to your list press 'A'
+#- To REMOVE an item from your list press 'R'
+#- To UPDATE the quantity of an item press 'U'
+#- To PRINT your list press 'P'
+#- To EXIT press 'E
+#A
+#What item would you like to add?
+#icecream
+#How many would you like to add?
+#4
+#icecream has been added with a quantity of 4.
+#- To CREATE a list press 'C'
+#- To ADD to your list press 'A'
+#- To REMOVE an item from your list press 'R'
+#- To UPDATE the quantity of an item press 'U'
+#- To PRINT your list press 'P'
+#- To EXIT press 'E
+#R
+#What item would you like to remove?
+#lemonade
+#lemonade has been removed from your list.
+#- To CREATE a list press 'C'
+#- To ADD to your list press 'A'
+#- To REMOVE an item from your list press 'R'
+#- To UPDATE the quantity of an item press 'U'
+#- To PRINT your list press 'P'
+#- To EXIT press 'E
+#U
+#What item would you like to update?
+#icecream
+#What is the new quantity you would like for icecream?
+#1
+#icecream has been updated to have a quantity of 1.
+#- To CREATE a list press 'C'
+#- To ADD to your list press 'A'
+#- To REMOVE an item from your list press 'R'
+#- To UPDATE the quantity of an item press 'U'
+#- To PRINT your list press 'P'
+#- To EXIT press 'E
+#P
+#"GROCERY LIST"
+#"apple : 1"
+#"cereal : 1"
+#"tomatoes : 3"
+#"onions : 1"
+#"icecream : 1"
+#- To CREATE a list press 'C'
+#- To ADD to your list press 'A'
+#- To REMOVE an item from your list press 'R'
+#- To UPDATE the quantity of an item press 'U'
+#- To PRINT your list press 'P'
+#- To EXIT press 'E
+#E
+#Thank you for using 'GROCERY LIST'
+
+ruby :>
