21bb59c3921fbe80f0ddb026947e2634b220bfe0
ADEsposito <a.desposito640@gmail.com>
2016-07-07 21:43:49 -0500

    release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,41 @@
+#module Shout
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+
+#  def self.yell_happily(words)
+#    words + "!!!" + " :)"
+#  end
+#end
+
+#Shout.yell_angrily("No")
+#Shout.yell_happily("Yes")
+
+# Make a mixin version of the Shout module
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    p words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
-    words + "!!!" + " :)"
+  def yell_happily(words)
+    p words + "!!!" + " :)"
   end
 end
 
-Shout.yell_angrily("No")
-Shout.yell_happily("Yes")
+# Write 2 classes representing anything that might shout, and include the Shout module in those classes
+class Girl
+  include Shout
+end
+
+class Boy
+  include Shout
+end
+
+# Driver code that instantiates instances of the classes and calls the 2 module methods on each instance
+girl = Girl.new
+girl.yell_angrily("I'm yelling angrily")
+girl.yell_happily("I'm yelling happily")
+
+boy = Boy.new
+boy.yell_angrily("I'm yelling more angrily than she was")
+boy.yell_happily("I'm yelling more happily than she was")
