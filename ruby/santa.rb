8646d5293c3583b9ca71b0099368cf537ce73a3d
ADEsposito <a.desposito640@gmail.com>
2016-07-07 20:30:11 -0500

    release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,13 +1,12 @@
 class Santa
-
-  attr_reader :age, :ethnicity
+  attr_reader  :age, :ethnicity
   attr_accessor :gender
-
-  def initialize(gender, ethnicity)
+  
+  def initialize(gender, ethnicity, age = 0)
     puts "Initializing Santa instance ..."
     @gender = gender
     @ethnicity = ethnicity
-    @age = 0
+    @age = age
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
@@ -20,19 +19,19 @@ class Santa
   end
 
   # getter methods for age, ethnicity, and gender attributes
-  # def age
-  #   @age
-  # end
+  #def age
+  #  @age
+  #end
 
-  # def ethnicity
-  #   @ethnicity
-  # end
+  #def ethnicity
+  #  @ethnicity
+  #end
 
-  # def gender
+  #def gender
   #  @gender
-  # end
+  #end
 
-  # attribute-changing methods
+  #attribute-changing methods
   def celebrate_birthday=(age)
     @age = @age + 1
     p @age
@@ -46,28 +45,41 @@ class Santa
   end
 
   # setter method for @gender attribute
-  # def gender=(new_gender)
+  #def gender=(new_gender)
   #  @gender = new_gender
-  # end
- end
+  #end
+end
 
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+santas = []
+
+1000.times do |i|
+  random_gender = example_genders.sample
+  random_ethnicity = example_ethnicities.sample
+  random_age = rand(0..140)
+  
+  santas << Santa.new(random_gender, random_ethnicity, random_age)
+  p santas[i].gender
+  p santas[i].ethnicity
+  p santas[i].age
+end
+  
+# MISC DRIVER CODE
 #santa = Santa.new
 #santa.eat_milk_and_cookies("snickerdoodle")
 #santa.speak
 
-#santas = []
-#example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-#example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 #example_genders.length.times do |i|
   #santas << Santa.new(example_genders[i], example_ethnicities[i])
 #end
-
-santa = Santa.new("female", "white")
-p "The #{santa.age} year old santa is a #{santa.ethnicity} #{santa.gender}."
-santa.gender = "male"
-p "santa is now a #{santa.gender}"
-santa.get_mad_at=("Rudolph")
-santa.celebrate_birthday=(@age)
-santa.celebrate_birthday=(@age)
-santa.get_mad_at=("Prancer")
-santa.celebrate_birthday=(@age)
+#santa = Santa.new("female", "white")
+#p "The #{santa.age} year old santa is a #{santa.ethnicity} #{santa.gender}."
+#santa.gender = "male"
+#p "santa is now a #{santa.gender}"
+#santa.get_mad_at=("Rudolph")
+#santa.celebrate_birthday=(@age)
+#santa.celebrate_birthday=(@age)
+#santa.get_mad_at=("Prancer")
+#santa.celebrate_birthday=(@age)
