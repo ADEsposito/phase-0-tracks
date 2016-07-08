5f0967416c644139e77b4a3a4f1bbd454b1ec4b8
ADEsposito <a.desposito640@gmail.com>
2016-07-07 21:20:14 -0500

    Release 1

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    words + "!!!" + " :)"
+  end
 end
+
+Shout.yell_angrily("No")
+Shout.yell_happily("Yes")
