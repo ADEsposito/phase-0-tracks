f912ddb4356328b6c8318d0a72f2f8ed81c132fb
ADEsposito <a.desposito640@gmail.com>
2016-07-14 15:40:25 -0500

    initial commit, with pseudocode

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// create function that takes a word as a parameter
+  // within function, create a variable that will hold the letters as they come back from the loop
+  // loop through each letter of word backwards
+    // store each letter in new variable
+  // close loop
+  // print new variable (which contains all letters of original word, but in reverse order)
+// close function
