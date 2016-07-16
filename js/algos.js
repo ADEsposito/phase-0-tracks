c8618a3410e4c71652e9e158f6e4fe93ac01e4fb
ADEsposito <a.desposito640@gmail.com>
2016-07-15 21:40:35 -0500

    add release 2 code

--- js/algos.js
+++ js/algos.js
@@ -35,6 +35,40 @@ function keyValueMatch(object1, object2) {
 	}
 }
 
+// Release 2: Generate Random Test Data
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomWord(integer) {
+	var text = "";
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	for (var i = 0; i < integer; i++)
+		text += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+	return text;
+}
+
+function randomTestData(number) {
+	testDataArray = [];
+	var arrayLength = number;
+	wordLength = getRandomInt(1, 10);
+	for (var i = 0; i < arrayLength; i++)
+		testDataArray.push(randomWord(getRandomInt(1,10)));
+	console.log(testDataArray);
+}
+
+// Release 2: Step 2 (add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result)
+for (var i = 1; i < 11; i++) {
+	randomTestData(i);
+	var longestWord = longestPhraseFinder(testDataArray);
+}
+
+// Driver code for randomTestData:
+//randomTestData(10);
+//randomTestData(2);
+//randomTestData(3);
+//randomTestData(5);
+
 //Driver code for keyValueMatch:
 //testObject1 = {name: "Steven", age: 54};
 //testObject2 = {name: "Tamir", age: 54};
