585fb46c67d01e84e3a1b8c90209916af71a4049
ADEsposito <a.desposito640@gmail.com>
2016-07-24 16:10:01 -0500

    add column for purchased status, add code for get input and methods to add items or exit program

--- databases/shopping_list.rb
+++ databases/shopping_list.rb
@@ -2,31 +2,65 @@
 require 'sqlite3'
 
 # create SQLite3 database
-database = SQLite3::Database.new("shopping.db")
+$database = SQLite3::Database.new("shopping.db")
 
 create_shopping_table = <<-SQL
   CREATE TABLE IF NOT EXISTS shopping(
     id INTEGER PRIMARY KEY,
     item VARCHAR(255),
     quantity INT,
-    store_id INT,
-    FOREIGN KEY (store_id) REFERENCES stores(id)
+    store_name VARCHAR(255),
+    purchased BOOLEAN
   )
 SQL
 
-create_stores_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS stores(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255)
-  )
-SQL
-
-create_stores_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS stores(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255)
-  )
-SQL
 # create shopping and stores table
-database.execute(create_shopping_table)
-database.execute(create_stores_table)
+$database.execute(create_shopping_table)
+
+# method to add item to list
+def add_to_list(database, item, quantity, store_name)
+  $database.execute("INSERT INTO shopping (item, quantity, store_name) VALUES (?, ?, ?)", [item, quantity, store_name])
+  $database.execute("INSERT INTO stores (name) VALUES (?)", [name])
+end
+
+# method to remove item from list
+
+# method to update quantity of item
+
+# method to update item to purchased status
+
+# method to display all items for a particular store
+
+# method to display list of all items and status (purchased/not purchased)
+
+def get_input
+  exit = false
+
+  until exit
+    puts "- To ADD to your list press'A'"
+    puts "- To REMOVE an item from your list press 'R'"
+    puts "- To UPDATE the quantity of an item press 'U'"
+    puts "- To PRINT your list press 'P'"
+    puts "- To EXIT press 'E'"
+
+    choice = gets.chomp
+
+    if choice == 'A'
+      puts "Please enter the item you wish to add :"
+      item = gets.chomp
+      puts "How many would you like to add?"
+      quantity = gets.chomp
+      puts "What store will you get #{item} from?"
+      name = gets.chomp
+      add_to_list($database, item, quantity, name)
+      exit = false
+    end
+
+    if choice == 'E'
+      puts "Thank you for using MY SHOPPING LIST"
+      exit = true
+    end
+  end
+end
+
+get_input
