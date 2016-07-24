9b11be63c0ca342c584980f205cdbc44b19fb9bb
ADEsposito <a.desposito640@gmail.com>
2016-07-24 13:27:07 -0500

    add execute command

--- databases/shopping_list.rb
+++ databases/shopping_list.rb
@@ -2,4 +2,17 @@
 require 'sqlite3'
 
 # create SQLite3 database
-database = SQLite3::Database.new("groceries.db")
+database = SQLite3::Database.new("shopping.db")
+
+create_shopping_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS shopping(
+    id INTEGER PRIMARY KEY,
+    item VARCHAR(255),
+    quantity INT,
+    store_id INT,
+    FOREIGN KEY (store_id) REFERENCES stores(id)
+  )
+SQL
+
+# create shopping table
+database.execute(create_shopping_table)
