51e7f2af4bdc1ed3abbc82479312305cc4e2625f
ADEsposito <a.desposito640@gmail.com>
2016-07-29 13:48:35 -0500

    make an erb template, create a route, build a form

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,24 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/delete' do
+  erb :delete_student
+end
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
+end
+
+# delete a student via
+# a form
+post '/xyz' do
+  #db.execute("UPDATE students SET age = (?) WHERE name = (?)", [params['age'].to_i, params['name']])
+  db.execute("DELETE FROM students WHERE name = (?)",
+  #[params['age'].to_i,
+  [params['name']])
+ redirect '/'
 end
 
 # add static resources
